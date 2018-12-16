96bddf18c23d49085744e7d84c06b527abd36c9f
otherness-space <the@otherness.space>
2018-12-16 16:01:08 -0500

    comments assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 // initialize sidenav
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+// Please note that the sidenav HTML should not be contained within the navbar HTML.
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+// $(document).ready(function(){
+//   $('.sidenav').sidenav();
+// });
