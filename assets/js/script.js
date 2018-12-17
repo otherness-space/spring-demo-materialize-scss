7f438360f8f740bd39958b58461548e137e2aec0
otherness-space <the@otherness.space>
2018-12-16 21:02:21 -0500

    moved all comments to init.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,1 @@
-// initialize sidenav
-// Please note that the sidenav HTML should not be contained within the navbar HTML.
 
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-// Or with jQuery
-
-// $(document).ready(function(){
-//   $('.sidenav').sidenav();
-// });
