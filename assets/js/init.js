95b8f7f4be85b86488f3ac713d3d1014849011e7
otherness-space <the@otherness.space>
2019-01-26 20:08:28 -0500

    floating bottom menu

--- assets/js/init.js
+++ assets/js/init.js
@@ -24,3 +24,37 @@ document.addEventListener('DOMContentLoaded', function() {
 //
 //   }); // end of document ready
 // })(jQuery); // end of jQuery name space
+
+// https://materializecss.com/floating-action-button.html#horizontal
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'top'
+  });
+});
+
+// Options
+// ____________________________________________________________________________
+// Name           | Type	  | Default | Description
+// _______________|_________|_________|________________________________________
+// direction	    | String	| 'top'	  | Direction FAB menu opens. Can be 'top',
+//                |         |         | 'right', 'buttom', 'left'
+// _______________|_________|_________|________________________________________
+// hoverEnabled	  | Boolean	| true	  | If true, FAB menu will open on hover
+//                |         |         | instead of click
+// _______________|_________|_________|________________________________________
+// toolbarEnabled	| Boolean	| false	  | Enable transit the FAB into a toolbar
+//                |         |         | on click
+// _______________|_________|_________|________________________________________
+
+// Properties
+// ____________________________________________________________________________
+// Name	   | Type	   | Description
+// ________|_________|_________________________________________________________
+// el	     | Element | The DOM element the plugin was initialized with.
+// ________|_________|_________________________________________________________
+// options | Object	 | The options the instance was initialized with.
+// ________|_________|_________________________________________________________
+// isOpen  | Boolean | Describes open/close state of the FAB.
+// ________|_________|_________________________________________________________
